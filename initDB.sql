5bf0c2ba855973c439676c64db266b7a2d7da751
Andrew Krause <ajkrause@gmail.com>
2024-02-24 19:42:32 -0500

    Add database setup script

---
+++ initDB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE url_shortener;
+
+USE url_shortener;
+
+CREATE TABLE IF NOT EXISTS urls (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    targetURL VARCHAR(2048) CHARACTER SET 'ascii' COLLATE 'ascii_general_ci' NOT NULL,
+    shortCode VARCHAR(50) CHARACTER SET 'ascii' COLLATE 'ascii_general_ci' NOT NULL,
+    dateCreated date NOT NULL,
+    dateLastHit date NOT NULL,
+    hits INT DEFAULT 0,
+    
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
